12ed0a1fa90df525fdb60bcd4d97f466be68c3e6
Konghyoeun <hn01360@naver.com>
2021-10-27 22:57:42 +0900

    feat: learn AppLoading

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { reloadAsync } from "expo-updates";
+import React, { useState } from "react";
+import { Text, Image } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [assets] = useAssets([require("./test.png")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!assets || !loaded) {
+    return <AppLoading />;
+  }
+  return <Text>We are done loading</Text>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
